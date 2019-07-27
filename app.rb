dee9e6651393c46d12c151fd1f7f707d70901706
Abagail Hudson <abagailanne@gmail.com>
2019-07-27 00:22:12 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,10 +7,10 @@ class App < Sinatra::Base
   end
 
   post '/reverse' do
-    
+
   original_string = params["string"]
   reversed_string = original_string.reverse
-  
+
     erb :reversed
   end
 
